7261fa1ecb25e9f7cb92cafde4ef9200a0281db2
Philippe Raemy <philippe_raemy@swissonline.ch>
2016-09-06 09:40:05 +0200

    initial commit

---
+++ Now/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Now
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Any(a=>a=="/?" || a== "-?"))
+            {
+                Console.WriteLine("Usage is now [--pipe] [--fmt=yyyy-MM-dd] \"message text\" where fmt is any valid dotnet DateTime format string and --pipe indicates to read from sysin.");
+                return;
+            }
+            var fmt = args
+                .Where(a => a.StartsWith("--fmt=", StringComparison.InvariantCultureIgnoreCase))
+                .Select(a => a.Split(new[] {'='}, 2).Skip(1).FirstOrDefault())
+                .FirstOrDefault()
+                      ?? "yyyy-MM-dd HH:mm:ss";
+
+            var msg=new StringBuilder();
+            var first = true;
+            var re=new Regex(@"^--\w+=?",RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+            foreach (var s in args
+                .Where(a => !re.Match(a).Success)
+            )
+            {
+                msg.Append(first ? " -- " : " ");
+                first = false;
+                msg.Append(s);
+            }
+            
+            if(args.Any(a => a.Equals("--pipe", StringComparison.InvariantCultureIgnoreCase)))
+            {
+                string line;
+                while ((line=Console.In.ReadLine())!=null)
+                {
+                    Console.WriteLine($"{DateTime.Now.ToString(fmt)}{msg} -- {line}");
+                }
+            }
+            else Console.WriteLine($"{DateTime.Now.ToString(fmt)}{msg}");
+        }
+    }
+}
---
+++ Now/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Now")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RaemyLalive&Kids")]
+[assembly: AssemblyProduct("Now")]
+[assembly: AssemblyCopyright("Copyright © RaemyLalive&Kids 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ecfbbf6d-e92e-4759-b207-3aae1eb756e9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
